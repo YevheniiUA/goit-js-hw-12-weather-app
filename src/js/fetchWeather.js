4bed0a5fbd8161ee6556e91df1d7f8ac422f9819
Evgenii <wojda007@gmail.com>
2019-07-18 13:23:41 +0300

    update

--- src/js/fetchWeather.js
+++ src/js/fetchWeather.js
@@ -1,6 +1,6 @@
 const fetchWeather = query => {
   const options = {
-    baseUrl: 'http://api.apixu.com/v1/current.json',
+    baseUrl: 'https://api.apixu.com/v1/current.json',
     apiKey: '5b3041f515dc4b54b04142808191107',
   };
 
